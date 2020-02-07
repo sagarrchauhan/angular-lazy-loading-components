4a7c7d97f56935fa266a470358f47ae5ae0fdc0a
Sagar Chauhan <sagar.chauhan@axelerant.com>
2020-02-07 11:14:08 +0530

    fix(component): add missing input members in components

--- src/app/components/comp3/comp3.component.ts
+++ src/app/components/comp3/comp3.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-comp3',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comp3.component.css']
 })
 export class Comp3Component implements OnInit {
+
+  @Input() data: any;
 
   constructor() { }
 
--- src/app/components/link/link.component.ts
+++ src/app/components/link/link.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 
@@ -8,6 +8,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./link.component.css']
 })
 export class LinkComponent implements OnInit {
+
+  @Input() data: any;
 
   constructor(private router: Router) { }
 
